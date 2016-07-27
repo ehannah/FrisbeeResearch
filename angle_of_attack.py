8d40297af4e46cd93b98a93d8a2b9a83806329d8
ehannah <ehannah@email.arizona.edu>
2016-07-27 10:49:37 -0700

    Add files via upload

---
+++ angle_of_attack.py
@@ -0,0 +1,31 @@
+'''
+The angle of attack is defined as the angle between the velocity of the disc
+and the plane of the frisbee.
+
+To calculate the angle of attack:
+
+1. Create lab-frame velocity vector ([vx,vy,vz])
+2. Determine which column or row of rotation matrix represents inertial coordinates
+of z-body unit vector
+3. Calculate dot product of z-body unit vector and velocity vector; this dot product
+is the scalar component of the velocity that is not in the plane of the disc
+4. Calculate the velocity in the plane of the disc by subtracting the above dot product times
+the z-body unit vector from the lab-frame velocity.
+
+'''
+import math
+import numpy as np
+import rotationmatrix as rm
+
+#Inertial coordinates of unit vectors correspond to columns of rotation matrix
+#Unit vectors are [1,0,0], [0,1,0], and [0,0,1], respectively
+
+xhat=rm.rotationmatrix(phi,theta).transpose()[0]
+yhat=rm.rotationmatrix(phi,theta).transpose()[1]
+zhat=rm.rotationmatrix(phi,theta).transpose()[2]
+
+def angle_of_attack(vx,vy,vz):
+	v_lab=np.array([vx,vy,vz])
+	z_component=np.dot(zhat,v_lab)
+	v_plane=v_lab-z_component*zhat
+	return math.atan(z_component/np.linalg.norm(v_plane)) #Not sure what the sign of arctan function should be
