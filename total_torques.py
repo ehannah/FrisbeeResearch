33f68bbc8fdffd8ef05eddacce3117e1a759be0f
ehannah <ehannah@email.arizona.edu>
2016-07-28 11:11:40 -0700

    Update total_torques.py

--- total_torques.py
+++ total_torques.py
@@ -4,3 +4,21 @@ Calculate each torque.
 1. Calculate angular velocities in frisbee frame.
 (Hummel Pg. 34: w=phidot*cos(theta)*xbhat+thetadot*ybhat+(phidot*cos(theta)+gammadot)*zbhat
 2. Calculate angular velocities in the lab 
+
+import numpy as np
+import math
+
+#Frisbee frame angular velocity
+
+def ang_velocitybod(theta, phidot, thetadot, gammadot):
+#Equation for inertial angular velocity can be found on pg. 34 of Hummel 2003:
+#w=phidot*cos(theta)*xhat+thetadot*yhat+(phidot*sin(theta)*gammadot)*zhat
+#Velocities are calculated here in frisbee frame
+
+  return np.array([phidot*math.cos(theta), thetadot, phidot*math.sin(theta)+gammadot)])
+
+'''
+#Lab frame angular velocity
+
+def ang_velocitylab
+'''
