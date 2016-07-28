5cbe07fbe0ae05af7653f4efbae5a275b2e792e5
ehannah <ehannah@email.arizona.edu>
2016-07-27 17:17:40 -0700

    Create total_torques.py

---
+++ total_torques.py
@@ -0,0 +1,6 @@
+'''
+Calculate each torque.
+
+1. Calculate angular velocities in frisbee frame.
+(Hummel Pg. 34: w=phidot*cos(theta)*xbhat+thetadot*ybhat+(phidot*cos(theta)+gammadot)*zbhat
+2. Calculate angular velocities in the lab 
