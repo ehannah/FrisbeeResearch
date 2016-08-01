8a2e7aedb48449d410b8fcc875a04c16596d8cfb
ehannah <ehannah@email.arizona.edu>
2016-08-01 11:31:08 -0700

    Update frisbee_object.py

--- frisbee_object.py
+++ frisbee_object.py
@@ -18,4 +18,12 @@ class Frisbee(object):
     
   def __str__(self):
     return 'Position: (' + str(self.x)+','+str(self.y)+','+str(self.z)+')'+'\n'
-      +'Velocity: ('+str(self.vx)+','+str(self.vy)+','+str(self.vz)+')'
+    +'Velocity: ('+str(self.vx)+','+str(self.vy)+','+str(self.vz)+')'
+
+  def rotationmatrix(self):
+    return np.array(
+  		[[math.cos(self.phi)*math.cos(self.theta), -math.sin(self.phi), -math.cos(self.phi)*math.sin(self.theta)],
+  		[math.sin(self.phi)*math.cos(self.theta), math.cos(self.phi), -math.sin(self.theta)*math.sin(self.phi)],
+  		[math.sin(self.theta), 0, math.cos(self.theta)]])
+  
+  def unitvectors(
