62922e144de7f432b1d58354c9f217fe849052a9
ehannah <ehannah@email.arizona.edu>
2016-08-05 16:57:16 -0700

    Added functions for unit vectors

--- frisbee_object.py
+++ frisbee_object.py
@@ -30,7 +30,27 @@ class Frisbee(object):
   		[[math.cos(self.phi)*math.cos(self.theta), -math.sin(self.phi), -math.cos(self.phi)*math.sin(self.theta)],
   		[math.sin(self.phi)*math.cos(self.theta), math.cos(self.phi), -math.sin(self.theta)*math.sin(self.phi)],
   		[math.sin(self.theta), 0, math.cos(self.theta)]])
-  	
+
+  #Below are unit important unit vectors which we will ultimately use to orient the forces
+  #and torques in the correct directions.
+
+  #Calculate unit vector in the direction of the disc's velocity (in lab frame).
+  def vhat(self):
+    return self.velocity/np.linalg.norm(self.velocity)
+
+  #Calculate unit vector in z-body direction. Corresponds to 3rd column of rotation matrix. 
+  def zbhat(self):
+    return np.transpose(self.rotationmatrix()[2])
+
+  #Calculate unit vector in x-body direction. Corresponds to unit vector in the direction
+  #of the plane of the disc (distinct from the velocity of the disc)
+  def xbhat(self):
+    zcomponent=np.dot(self.velocity,self.zbhat())
+    return self.velocity-self.zbhat()*zcomponent
+
+  def ybhat(self):
+    return np.cross(self.zbhat(),self.xbhat())
+
   #Calculate angle of attack, defined as angle between plane of disc and velocity vector
   #of the frisbee's motion. First step is to calculate scalar component of the velocity 
   #that is not in the plane of the disc (dot product of z-body unit vector and velocity
@@ -39,8 +59,6 @@ class Frisbee(object):
   #disc. The angle of attack is the angle formed between the z-component and the velocity
   #in the plane of the disc, which we calculate using an arctan function.
   def attackangle(self):
-    zbhat=np.transpose(self.rotationmatrix()[2])
-    zcomponent=np.dot(self.velocity,zbhat)
+    zcomponent=np.dot(self.velocity,self.zbhat())
     v_plane=self.velocity-zbhat*zcomponent
     return math.atan(zcomponent/np.linalg.norm(v_plane))
-
