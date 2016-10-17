f98aeed3acd76914540ab141b550e67ea3538f2c
ehannah <ehannah@email.arizona.edu>
2016-10-17 11:39:38 -0700

    Added out_file.close() call to close text file being written

--- simulatedthrow.py
+++ simulatedthrow.py
@@ -22,3 +22,5 @@ for element in time_position_uncertainty:
 		line=element[0]+' '+element[1]+' '+element[2]+' '+element[3]+' '+element[4]+' '+element[5]+' '+element[6]+' '
 		out_file.write(line)
 	count+=1
+
+out_file.close()
